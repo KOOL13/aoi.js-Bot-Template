af31a7abecd136206378f6dab5960cb790674c55
KOOL <82057676+KOOL13@users.noreply.github.com>
2021-04-26 18:56:46 -0500

    Create prefix.js

---
+++ commands/utility-commands/prefix.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'prefix',
+  code: `$setServerVar[prefix;$message]
+Prefix has been set to: \`$message\`
+$onlyIf[$message!=;Current prefix is: \`$getServerVar[prefix]\`.
+To change the prefix, do \`$getServerVar[prefix]prefix <prefix>\`]
+$onlyPerms[admin;You don't have permission to do that!]
+`
+}
