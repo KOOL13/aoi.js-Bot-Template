75e81d1d8d593276bec63ee118b028522801f34b
KOOL <82057676+KOOL13@users.noreply.github.com>
2021-05-05 14:31:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Make sure to check the README.md for other examples/usage
 
-const dbd = require("dbd.js")
-const bot = new dbd.Bot({
+const aoijs = require("dbd.js")
+const bot = new aoijs.Bot({
   token: 'YOUR BOT TOKEN HERE',
   prefix: [`$getServerVar[prefix]`, '<@$clientID>', '<@!$clientID>'], // The server prefix, and mentioning the bot will both work as a prefix
   mobile: false //Whether or not the bot has a mobile status
@@ -16,15 +16,12 @@ bot.onMessage({
 // Command Handler
 bot.loadCommands('./commands')
 
-//Bot Status
-
-// For single status:
+//Bot Status (For more info check README)
 bot.status({
   status: 'online', // options: online (if want mobile status, check above), idle, dnd, invisible 
   type: 'PLAYING', // options: WATCHING, PLAYING, LISTENING, COMPETING, STREAMING (if you choose streaming, you can also add the url: '' property)
   text: `on $serverCount servers`
 })
-// For multiple status just have multiple bot.status but add the time: <number> property.
 
 // Variables
 bot.variables({
